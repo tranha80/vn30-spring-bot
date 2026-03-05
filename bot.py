0f4046defa5482b44fb8f3d6f7f430a74ec10fef
tranha80 <35252955+tranha80@users.noreply.github.com>
2026-03-05 16:00:52 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,55 +5,52 @@ import pandas as pd
 TOKEN = "8543130885:AAGf4e2BOclCnRdFR2bSCe0fUMhj0jPXufs"
 CHAT_ID = "7084665160"
 
-
-def send_telegram(msg):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    data = {
-        "chat_id": CHAT_ID,
-        "text": msg
-    }
-    requests.post(url, data=data)
-
-
-VN30 = [
-"ACB.VN","BCM.VN","BID.VN","BVH.VN","CTG.VN","FPT.VN","GAS.VN","GVR.VN",
-"HDB.VN","HPG.VN","MBB.VN","MSN.VN","MWG.VN","PLX.VN","POW.VN","SAB.VN",
-"SHB.VN","SSB.VN","SSI.VN","STB.VN","TCB.VN","TPB.VN","VCB.VN","VHM.VN",
-"VIB.VN","VIC.VN","VJC.VN","VNM.VN","VPB.VN","VRE.VN"
+symbols = [
+"ACB.VN","BCM.VN","BID.VN","BVH.VN","CTG.VN","FPT.VN","GAS.VN","GVR.VN","HDB.VN","HPG.VN",
+"MBB.VN","MSN.VN","MWG.VN","PLX.VN","POW.VN","SAB.VN","SHB.VN","SSB.VN","SSI.VN","STB.VN",
+"TCB.VN","TPB.VN","VCB.VN","VHM.VN","VIB.VN","VIC.VN","VJC.VN","VNM.VN","VPB.VN","VRE.VN"
 ]
+
+def send_telegram(message):
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    requests.post(url, data=payload)
 
 
 def check_stock(symbol):
 
-    df = yf.download(symbol, period="3mo", interval="1d")
+    df = yf.download(symbol, period="6mo", interval="1d")
 
-    if df.empty:
+    if len(df) < 30:
         return
 
-    spring = df.iloc[-1]
-    prev = df.iloc[-20:-3]
+    spring_low = df['Low'].iloc[-2]
+    recent_low = df['Low'].iloc[-20:-3].min()
 
-    low_test = spring["Low"] < prev["Low"].min()
-    vol_test = spring["Volume"] > prev["Volume"].mean() * 1.5
-    bull_candle = spring["Close"] > spring["Open"]
+    spring_volume = df['Volume'].iloc[-2]
+    avg_volume = df['Volume'].iloc[-20:-3].mean()
 
-    if low_test and vol_test and bull_candle:
+    if spring_low < recent_low and spring_volume > avg_volume * 1.5:
 
-        entry = round(spring["High"] * 1.01, 2)
+        price = df['Close'].iloc[-1]
 
-        msg = f"""
+        message = f"""
 SPRING DETECTED
 
 Stock: {symbol}
-Close: {spring['Close']}
-Volume: {spring['Volume']}
-
-Suggested BUY STOP: {entry}
+Price: {price}
 """
-        send_telegram(msg)
+
+        send_telegram(message)
 
 
-for s in VN30:
+for s in symbols:
     try:
         check_stock(s)
     except:
